af7131bb263f8f3503aebe8bc5feba62f9729db9
SamDs <sam.ds@live.com>
2022-01-08 10:17:36 +0100

    reworked 01

--- e01/01-lychrel/Solution.java
+++ e01/01-lychrel/Solution.java
@@ -1,36 +1,85 @@
+import java.nio.ReadOnlyBufferException;
+import java.util.Scanner;
+
 // Numeri di Lychrel
 
 
 class Solution{
-    public static void main(String[] args) {   
-        lychrel(args[0]);
+    public static void main(String[] args) {  
+        Scanner s = new Scanner(System.in);
+        long input = s.nextLong(); 
+        s.close();
+        lychrel(input);
     }
     
     /**
      * Requires: Any positive number as a string 
      * Modifies: Modifies System.out
-     * Effects: Prints the Lychrel sequence of given number
+     * Effects: Prints to System.out the Lychrel sequence of given number S
      */
-    public static void lychrel(String s){
-        long l = Long.parseLong(s);
-        long n = 0L;
+    public static void lychrel(long input){
+        long l = input;
+        long n = 0L;        
 
         while(true){
-            n = Long.parseLong(swap(s));
-            System.out.printf("%d\n", l);
+            n = reverse(l);
+            System.out.println(l);
             if (n==l) break;
             l += n;
-            s = Long.toString(l); 
+           
         }
+
     }
     /**
      * Requires: Any string != null
      * Modifies: _
-     * Effects: Returns a reverse order string of input
+     * Effects: Returns a reverse order string of input S
      */
-    public static String swap(String s){        
+
+    /* 
+    public static long swap(String s){        
         int l = s.length();        
         if (l<=1) return s;    
         return swap(s.substring(1)) + s.charAt(0);
+    }
+    */
+
+
+
+    /**
+     * Requires: Any long >= 0
+     * Modifies: _
+     * Effects: Returns a long which is obtained by reversing input
+     *          Es. 1234L -> 4321L
+     */
+    public static long reverse(long input){
+        long output = reverse(input,lengthOfInput(input)-1);
+        //System.out.println("output: "+output);
+        return output;
+    }
+
+    /**
+     * Requires: Any long >= 0
+     * Modifies: _
+     * Effects: Returns the number of digits of input interpreted as a decimal number
+     *          Es. 1234L -> 4
+     */
+
+    public static int lengthOfInput(long input){
+        if(input == 0) return 0;
+        return 1+lengthOfInput(input/10);
+    }
+
+    /**
+     * Requires: Any long >= 0 and integer i >= 0
+     * Modifies: _
+     * Effects: Returns a long which is obtained by reversing input
+     *          Es. 1234L -> 4321L
+     */
+
+    public static long reverse(long input, int i){
+        //System.out.println("input:"+input+" i:"+i);
+        if(input==0 || i<0) return 0;       
+        return input%10 * (long) Math.pow(10, i) + reverse(input/10,--i);
     }
 }
