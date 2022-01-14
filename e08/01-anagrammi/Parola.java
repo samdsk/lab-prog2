f7609fef26aa80bfa0a0c0775d2505a717c7d515
SamDs <sam.ds@live.com>
2022-01-14 09:26:27 +0100

    comments

--- e08/01-anagrammi/Parola.java
+++ e08/01-anagrammi/Parola.java
@@ -2,14 +2,25 @@ import java.util.Arrays;
 import java.util.Objects;
 
 public class Parola {
+    //Overview it's immutable, represent a word 
+    //which contains a string made by reording charactors in ascending order.
 
     /**
      * AF(p) = p.word 
      * RI(p) = p.word != null && p.sorted_word != null;
      */
+
+    //fields
+    //the word that class represents
     protected final String word;
+    //rapresents the word where charactors are reordered in ascending order
+    //Es. dcab -> abcd
     protected final String sorted_word;
 
+    /**
+     * Initialize this as word = s and sorted_word = reordered word
+     * if s is null throws NullPointerException
+     */
     public Parola(String s) throws NullPointerException{
         if(s == null) throw new NullPointerException();
         this.word = s;
@@ -18,6 +29,10 @@ public class Parola {
         assert repOk();
     }
 
+    /**
+     * Initialize this as the given Parola s
+     * throws NullPointerException if s is null
+     */
     public Parola(Parola s)throws NullPointerException{
         if(s == null) throw new NullPointerException();
         this.word = s.word;
@@ -26,6 +41,10 @@ public class Parola {
         assert repOk();
     }
 
+    /**
+     * Effects: Returns a new string where the given s is reordered in ascending order
+     *          throws NullPointerException if s == null
+     */
     public String sort(String s)throws NullPointerException{
         if(s == null) throw new NullPointerException();
         char[] temp = s.toCharArray();
@@ -33,11 +52,18 @@ public class Parola {
         return new String(temp);
     }
 
+    /**
+     * Effects: Return true if the given Parola p is an anagram of this
+     *          throws NulPointerException if p == null
+     */
     public boolean isAnagram(Parola p)throws NullPointerException{
         if(p == null) throw new NullPointerException();
         return sorted_word.equals(p.sorted_word);
     }
 
+    /**
+     * RI
+     */
     public boolean repOk(){
         if(word != null && sorted_word != null) return true;
         return false;
--- e08/01-anagrammi/Parole_Anagrammi.java
+++ e08/01-anagrammi/Parole_Anagrammi.java
@@ -1,25 +1,40 @@
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 
 public class Parole_Anagrammi extends Parola{
+    //Overview extends Parola class by adding a method to build 
+    //a list of parola which are anagrams of super
 
     /**
      * AF(a) = [Parola_0,...Parola_n] where Parola_i are anagrams of Parola_0 where i is 0<=i<size
      * 
-     * RI = super.repOk() && size==list.size=>1 && list.get(0).word == this.word
+     * RI = super.repOk() && size==list.size && size=>1 && list.get(0).word == this.word
      */
 
+    //fields
+    // List of Parola in which we store anagrams
     private List<Parola> list;
+    // the number of anagrams, it is always >= 1 because of reflexive property of an anagram
     private int size;
 
-    public Parole_Anagrammi(Parola s) throws NullPointerException {
+    /**
+     * Initialize super as Parola s, this.list as a list containing s.word and size = 1
+     */
+    public Parole_Anagrammi(Parola s){
         super(s);
         list = new LinkedList<>();
         list.add(s);        
         size = 1;
     }
 
+    /**
+     * Modifies: this and the given List l
+     * Effects: If the given List l has anagrams of this, they will be 
+     *          appened to this.list (only once, no duplicates), this.size increased by 1 and removed from List l and returns the modified l.
+     *          Throws NullPointerException if List l is null.
+     */
     public List<Parola> Trova_Anagrammi(List<Parola> l)throws NullPointerException {      
         
         Iterator<Parola> it = l.iterator();
@@ -37,32 +52,62 @@ public class Parole_Anagrammi extends Parola{
         return l;
     }
 
+    /**
+     * Retun the size of this.list
+     * @return size
+     */
+
     public int size(){
         return size;
     }
 
+    /**
+     * Returns this.list of anagrams
+     * @return list
+     */
     public List<Parola> getList(){
         return list;
     }
 
-    //RI = super.repOk() && size==list.size=>1 && list.get(0).word == this.word
-    
+    //RI = super.repOk() && size==list.size && size=>1 && list.get(0).word == this.word    
     @Override
     public boolean repOk(){
         if(super.repOk()){
             if(size==list.size() 
-            && size >= 0
+            && size >= 1
             && list.get(0).word.equals(super.word)) return true;
         }
 
         return false;
     }
 
-
-
     @Override
     public String toString() {        
         return list.toString();
+    }
+
+    @Override
+    public boolean equals(Object obj)throws NullPointerException {
+        if(obj == null) throw new NullPointerException();
+        if(obj == this) return true;
+        if(!(obj instanceof Parole_Anagrammi)) return false;
+
+        Parole_Anagrammi p = (Parole_Anagrammi) obj;
+
+        if(!super.equals(p)) return false;
+
+        for (Parola p1 : list) {
+           for (Parola p2 : p.list) {
+               if(!p1.equals(p2)) return false;
+           }
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {        
+        return Objects.hash(list,size,word,sorted_word);
     }
 
 
