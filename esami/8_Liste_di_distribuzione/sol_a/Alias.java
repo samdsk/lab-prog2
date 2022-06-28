712ef3b21cccfca9b12d2d9b0a9090f39bad1c3f
Sam. K <sam.ds@live.com>
2022-06-28 13:47:37 +0200

    alias

--- esami/8_Liste_di_distribuzione/sol_a/Alias.java
+++ esami/8_Liste_di_distribuzione/sol_a/Alias.java
@@ -1,31 +1,168 @@
-import java.util.HashMap;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
-public class Alias {
+public class Alias implements Iterable<Indirizzo> {
     /**
      * Overview: Alias è una classe concreta mutabile 
      * e rappresenta un elenco senza duplicati di indizzi email dello stesso dominio
      * 
-     * AF = 
+     * AF = Alias nome = 
+     *      indirizzo_1
+     *      ...
+     *      indirizzo_n
      * 
      * RI = elenco != null
      *      && nome != null
      *      && nome.length > 0
      *      && dominio != null
      *      && elenco non abbiano duplicati
-     *      && tutti gli elementi del elenco devono avere dominio == this.dominio
+     *      && tutti gli elementi dell'elenco devono avere dominio == this.dominio
      * 
      */
 
     final private Set<Indirizzo> elenco;
     final private String nome;
     final private Dominio dominio;
-    
 
-    public String getName();
-    public String getDomain();
-    public void add(Indirizzo i);
-    public void remove(Indirizzo i);
-    public boolean contains(Indirizzo i)
+
+    /**
+     * Inizializza un Alias avente un elenco vuoto con il dato nome e dominio
+     * @param nome il nome dell'alias
+     * @param dominio il dominio del dominio
+     * @throws NullPointerException se il nome oppure dominio è null
+     * @throws IllegalArgumentException se il nome è vuoto
+     */
+    public Alias(final String nome, final Dominio dominio){
+        if(!(Objects.requireNonNull(nome, "Nome non può essere null!").length()>0)) 
+            throw new IllegalArgumentException("Nome dell'alias non può essere vuoto");
+        
+        Objects.requireNonNull(dominio,"Dominio non può essere null!");
+
+        elenco = new HashSet<>();
+        this.nome = nome;
+        this.dominio = new Dominio(dominio.dominio());
+
+        assert repOk();
+    }
+
+    /** RI */
+    public boolean repOk(){
+        if(elenco != null
+            && nome != null
+            && nome.length()>0
+            && dominio != null
+        ){
+            for (Indirizzo i : elenco) {
+                if(!i.dominio().equals(this.dominio)) return false;
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+
+    /**
+     * Restituisce il nome questo alias
+     * @return
+     */
+    public String getName(){
+        return nome;
+    }
+
+    /**
+     * Restituisce il dominio di questo alias
+     * @return
+     */
+    public Dominio getDomain(){
+        return dominio;
+    }
+
+    /**
+     * Aggiunge un nuovo indirizzo email a questo alias 
+     * e restituisce true se è stato aggiungto all'alias altrimenti false
+     * @param i l'indirizzo email
+     * @return true se stato aggiungo a questo alias false altrimenti
+     * @throws NullPointerException se l'indirizzo è null
+     * @throws IllegalArgumentException se l'indirizzo ha un dominio diverso da quello di questo alias
+     */
+    public boolean add(Indirizzo i) throws NullPointerException,IllegalArgumentException{
+        Objects.requireNonNull(i,"L'indirizzo non può essere null!");
+        if(!i.dominio().equals(this.dominio)) throw new IllegalArgumentException("Dominio dell'indirizzo fornito non combacia con il dominio dell'alias");
+        
+        if(!elenco.contains(i)) elenco.add(i);
+        else return false;
+
+        assert repOk();
+        return true;
+    }
+    /**
+     * Rimuove l'indirizzo dato se contenuto in questo alias e
+     * restituisce true se è stato rimosso da questo alias false altrimenti
+     * @param i l'indirizzo da rimuovere
+     * @return true se è stato rimosso false altrimenti
+     * @throws NullPointerException se l'indirizzo è null
+     * @throws IllegalArgumentException se l'indirizzo da rimuovere ha un dominio diverso da questo alias
+     */
+    public boolean remove(Indirizzo i) throws NullPointerException,IllegalArgumentException{
+        Objects.requireNonNull(i,"L'indirizzo non può essere null!");
+        if(!i.dominio().equals(this.dominio)) throw new IllegalArgumentException("Dominio dell'indirizzo fornito non combacia con il dominio dell'alias");
+        
+        if(elenco.contains(i)) elenco.remove(i);
+        else return false;
+
+        assert repOk();
+        return true;
+    }
+
+    /**
+     * Restituisce true se il dato indirizzo è contenuto in questo alias false altrimenti
+     * @param i l'indirizzo da controllare
+     * @return true se stato trovato nell'alias false altrimenti
+     * @throws NullPointerException se l'indirizzo è null
+     */
+    public boolean contains(Indirizzo i) throws NullPointerException{
+        Objects.requireNonNull(i,"L'indirizzo non può essere null!");
+        return elenco.contains(i);
+    }
+
+    @Override
+    public Iterator<Indirizzo> iterator() {
+
+        final List<Indirizzo> list = new ArrayList<>(elenco);
+        
+        Collections.sort(list,
+            new Comparator<Indirizzo>() {
+
+                @Override
+                public int compare(Indirizzo o1, Indirizzo o2) {
+                    return o1.toString().compareTo(o2.toString());
+                }
+            }
+        );
+
+        Iterator<Indirizzo> it = Collections.unmodifiableList(list).iterator();
+
+        return new Iterator<Indirizzo>() {
+            @Override
+            public boolean hasNext() {
+                return it.hasNext();
+            }
+
+            @Override
+            public Indirizzo next() {
+                return it.next();
+            }
+            
+        };
+    }
 
 }
--- esami/8_Liste_di_distribuzione/sol_a/Indirizzo.java
+++ esami/8_Liste_di_distribuzione/sol_a/Indirizzo.java
@@ -1,17 +1,14 @@
 import java.util.Objects;
-import java.util.function.Predicate;
-import java.util.regex.Pattern;
-
-public record Indirizzo(String locale,String dominio) {
+public record Indirizzo(Locale locale,Dominio dominio){
     /**
-     * Overview: Indirizzo un classe immutabile e rappresenta un indirizzo email
+     * Overview: Indirizzo un classe immutabile e rappresenta un indirizzo email 
+     *  formato dalle parti locale e dominio
      * 
      * 
      * AF = locale@dominio
      * 
-     * RI = locale != null && locale.length > 0
-     *      && dominio != null && dominio.length > 0
-     *      && validator(locale,dominio)
+     * RI = locale != null
+     *      && dominio != null
      */
 
     /**
@@ -19,19 +16,11 @@ public record Indirizzo(String locale,String dominio) {
      * @param locale la parte locale dell'indirizzo email
      * @param dominio la parte del dominio dell'indirizzo email
      * @throws NullPointerException se locale o dominio è null
-     * @throws IllegalArgumentException se locale o dominio sono vuote, 
-     *      oppure se locale e dominio non forma un indirizzo email valido
      */
-    public Indirizzo(final String locale, final String dominio){
-        if(!(Objects.requireNonNull(locale,
-            "La parte locale dell'indirizzo email non può essere null!").length()>0))
-            throw new IllegalArgumentException("La parte locale dell'indirizzo email non può essere vuota!");
-        if(!(Objects.requireNonNull(dominio,
-            "La parte del dominio dell'indirizzo email non può essere null!").length()>0))
-            throw new IllegalArgumentException("La parte del dominio dell'indirizzo email non può essere vuota!");
+    public Indirizzo(final Locale locale,final Dominio dominio){
+        Objects.requireNonNull(locale,"La parte locale dell'indirizzo email non può essere null!");
+        Objects.requireNonNull(dominio, "La parte del dominio dell'indirizzo email non può essere null!");
         
-        validator(locale, dominio);
-
         this.dominio = dominio;
         this.locale = locale;
 
@@ -41,35 +30,16 @@ public record Indirizzo(String locale,String dominio) {
 
     /*RI */
     private boolean repOk() {
-        return false;
+        return locale != null && dominio != null;
     }
-    /**
-     * Controlla se le due stringhe locale e dominio fornite forma un indirizzo email valido 
-     * @param locale la parte locale dell'indirizzo email
-     * @param dominio la parte del dominio dell'indirizzo email
-     * @throws IllegalArgumentException se locale e dominio non forma un indirizzo email valido
-     */
-    private static void validator(final String locale, final String dominio) throws IllegalArgumentException{
-
-        String er_locale = "^[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*$";
-        String er_dominio = "^[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
-
-        final Predicate<String> VALIDO_L = Pattern.compile(er_locale).asPredicate();
-        final Predicate<String> VALIDO_D = Pattern.compile(er_dominio).asPredicate();
-
-        if(!VALIDO_L.test(locale)) throw new IllegalArgumentException("La parte locale dell'indirizzo non valido!");
-        if(!VALIDO_D.test(dominio)) throw new IllegalArgumentException("La parte del dominio dell'indirizzo non valido!");
-
-        return;
-    }
-
+   
     @Override
     public String toString(){
         StringBuilder str = new StringBuilder();
 
-        str.append(locale);
+        str.append(locale.locale());
         str.append("@");
-        str.append(dominio);
+        str.append(dominio.dominio());
 
         return str.toString();
     }
@@ -87,4 +57,9 @@ public record Indirizzo(String locale,String dominio) {
     public int hashCode(){
         return Objects.hash(locale,dominio);
     }
+
+    public int compareTo(Indirizzo o2) {
+        return 0;
+    }
+
 }
