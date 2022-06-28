674e73845aea48f7a9a1aa2eecec69d80e6ead41
Sam. K <sam.ds@live.com>
2022-06-28 19:26:18 +0200

    organize

---
+++ esami/5_playfy/sol_a/Album.java
@@ -0,0 +1,14 @@
+public class Album {
+    
+    public Brano getBrano(final String brano){
+
+    }
+
+    public Brano getBrano(final int pos){
+
+    }
+
+    public int posizione(Brano b){
+        
+    }
+}
---
+++ esami/5_playfy/sol_a/Durata.java
@@ -0,0 +1,14 @@
+import java.util.Objects;
+
+public class Durata{
+    private int secondi;
+
+    public Durata(final int sec){
+        Objects.requireNonNull(arr,"Array arr non può essere null!");
+
+        
+    }
+
+
+    private static int convert()
+}
